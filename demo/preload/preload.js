2d7258ca87598609762445a39516744e36f830e3
flavienliger <fliger@altera-group.fr>
2014-02-04 14:25:30 +0100

    Add displayPreload (simply preload bar) / library rework

--- demo/preload/preload.js
+++ demo/preload/preload.js
@@ -31,14 +31,17 @@
 			that.iLoad = 0;
 			that.iTotal = 0;
 			
-			function imageLoad(s){
+			var imageLoad = function(s){
+				if(typeof s !== 'string')
+					return console.error('imageLoad needed string '+s);
+				
 				i = new Image();
 				i.src = that.sRoot+s;
 				i.onerror = function(){ that.bError = true; };
 				i.onabort = function(){ that.bError = true; };
 				i.onload = function(){ that.iLoad++; };
 				return i;
-			}
+			};
 			
 			for(key in oImage){
 				oImage[key] = imageLoad(oImage[key]);
@@ -58,6 +61,33 @@
 			return oImage;
 		}; 
 	
+		this.displayPreload = function(){
+			
+			document.body.innerHTML += 
+				'<div class="preload"><div class="preload-bar"></div></div><div class="overlay"></div>';
+			
+			var preload = window.getComputedStyle(document.getElementsByClassName('preload')[0], null);
+			var width = parseInt(preload.getPropertyValue("width"), 10);
+			var padding = parseInt(preload.getPropertyValue("padding-right"), 10);
+			var size = width - padding;
+			
+			var bar = document.getElementsByClassName('preload-bar')[0];
+			var info = {};
+			var nPx = 0;
+			
+			var interval = setInterval(function(){
+				
+				info = that.getLoad();
+				nPx = size/info.total;
+				bar.style.width = info.load*nPx+'px';
+				
+				if(info.complete){
+					//document.getElementsByClassName('preload')[0].remove();	
+					clearInterval(interval);
+				}
+			}, 500);
+		};
+		
 		this.getLoad = function(aMessage){
 			
 			var r = '';
