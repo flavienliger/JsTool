3a38728db6249077b6a41364b7da91f5c9bce46a
flavienliger <fliger@altera-group.fr>
2014-02-07 17:47:37 +0100

    base physic
    http://gamedevelopment.tutsplus.com/tutorials/create-custom-2d-physics-engine-aabb-circle-impulse-resolution--gamedev-6331

---
+++ wip/physic/script.js
@@ -0,0 +1,30 @@
+var AABB = {
+	min: new Vec2(min),
+	max: new Vec2(max)
+};
+
+function AABBvsAABB(a, b){
+	if(a.max.x < b.min.x || a.min.x > b.max.x) return false;
+	if(a.max.y < b.min.y || a.min.y > b.max.y) return false;
+	
+	return true;
+}
+
+var Circle = {
+	radius: 0,
+	x: 0,
+	y: 0
+};
+
+var CirclevsCircle(a, b){
+	var r = a.radius+b.radius;
+	r*=r;
+	return r < Math.pow(a.x+b.x, 2)+Math.pow(a.y+b.y, 2);
+}
+
+V^AB = V^B-V^A
+= relative velocity
+le vecteur est le résultat de la fin moins le début, 
+
+V^AB*n = (V^B-V^A)*n
+n = normal
