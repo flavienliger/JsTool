1538b40b299de026c279db4cb29dfce2ed17f1fa
lieo <FLIGER@MTPPC01.altera-group.local>
2015-04-29 16:41:24 +0200

    hexToRgb

--- src/tools.js
+++ src/tools.js
@@ -1,4 +1,4 @@
-    (function(){
+(function(){
     'use strict';
 
     /**
@@ -452,6 +452,15 @@
         return deg*Math.PI/180;  
     };
     
+    var hexToRgb = function(hex) {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        } : null;
+    };
+        
     window.utils = {
         round: roundNumber,
         Range: Range,
